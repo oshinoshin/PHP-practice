8dac6f6db99ab0803487016e57a3f1b57ae74b85
oshinoshin <donateluro@gmail.com>
2021-05-11 08:53:38 +0900

    Create 3-3.php

---
+++ practice3/3-3.php
@@ -0,0 +1,30 @@
+<?php
+
+class Post
+{
+  public $text;
+  public $likes;
+
+  public function __construct($text, $likes)
+  {
+    $this->text = $text;
+    $this->likes = $likes;
+  }
+
+  public function show()
+  {
+    printf('%s (%d)' . PHP_EOL, $this->text, $this->likes);
+  }
+}
+
+$posts = [];
+$posts[0] = new Post('hello', 0);
+// $posts[0]->text = 'hello';
+// $posts[0]->likes = 0;
+
+$posts[1] = new Post('hello again', 0);
+// $posts[1]->text = 'hello again';
+// $posts[1]->likes = 0;
+
+$posts[0]->show();
+$posts[1]->show();
